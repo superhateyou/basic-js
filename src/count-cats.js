136c5c824928043959945924898bbd61e35519e6
Pavel Serzhan <p.i.serzhan@gmail.com>
2020-09-07 17:43:43 +0300

    feat: count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,3 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(catspajamas) {
+  return catspajamas.flat().reduce((sum, elem) => elem === '^^' ? sum + 1 : sum ,0)
 };
