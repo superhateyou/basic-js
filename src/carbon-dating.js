fa375e50745bbee4e1c78498ab72581e13af71d3
Pavel Serzhan <p.i.serzhan@gmail.com>
2020-09-07 20:35:08 +0300

    feat: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,8 @@
-const CustomError = require("../extensions/custom-error");
-
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function dateSample(sampleActivity) {
+  return typeof sampleActivity !== "string" || +sampleActivity <= 0 || +sampleActivity > 15 || isNaN(parseFloat(sampleActivity))
+         ? false
+         : Math.ceil((Math.log(MODERN_ACTIVITY/(parseFloat(sampleActivity)))/0.693*HALF_LIFE_PERIOD))
+ };
