c07a0ecbee86799eec0a3e66e7ac4a73bba70d62
Pavel Serzhan <p.i.serzhan@gmail.com>
2020-09-12 21:58:26 +0300

    feat: recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,4 +2,5 @@ module.exports = class DepthCalculator {
     calculateDepth(arr) {
         return arr instanceof Array ? (arr.length ? Math.max(...arr.map(x => this.calculateDepth(x))) + 1 : 1) : 0;
         }
+        //fix commit 
 }
