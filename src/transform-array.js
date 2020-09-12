b4c56f585935210bba1f6cb9603fce7cefef1af0
Pavel Serzhan <p.i.serzhan@gmail.com>
2020-09-12 21:56:49 +0300

    feat: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,31 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+  if (!(arr instanceof Array)) throw new Error('wrong type');
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  const result = [];
+  let deleted;
+
+  for (let i = 0; i < arr.length; i++){
+    switch (arr[i]) {
+      case '--discard-next':
+        deleted = i + 1;
+        i++;
+        break;
+      case '--discard-prev':
+        if (result.length && deleted !== i - 1)
+        result.pop();
+        break;
+      case '--double-next':
+       if (i < arr.length - 1)
+        result.push(arr[i + 1]);
+        break;
+      case '--double-prev':
+        if (result.length && deleted !== i - 1)
+        result.push(arr[i - 1]);
+        break;
+      default:
+        result.push(arr[i]);
+    }
+  }
+
+return result;
+}
